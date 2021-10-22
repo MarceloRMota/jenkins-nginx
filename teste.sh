6d1466f56f33caf0b8c37779433d03655998f151
Mmota <marcelo.rmota@hotmail.com>
2021-10-22 18:59:20 +0000

    Alterações no image

--- teste.sh
+++ teste.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 cd terraform
-curl "http://$(terraform output |grep IP| sed 's/IP//g'|sed 's/,//g'|sed 's/ //g'|sed 's/"//'|sed 's/"//')/home"
+
+CURL=$(curl "http://$(terraform output |grep IP| sed 's/IP//g'|sed 's/,//g'|sed 's/ //g'|sed 's/"//'|sed 's/"//')")
+
+regex='Welcome to nginx!' 
+
+if [ $CURL == $regex ] 
+then 
+   echo "nginx no ar"
+else
+   echo "nginx fora"
+fi
