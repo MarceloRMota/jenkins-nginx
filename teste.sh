f605f177e041fe6c0ea54802530c26a540fc2240
Mmota <marcelo.rmota@hotmail.com>
2021-10-22 18:22:47 +0000

    s

--- teste.sh
+++ teste.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd terraform
-curl "http://$(terraform output | grep ec2 | awk '{print $2;exit}' |sed -e "s/\",//g"|cut -b 8-)"
+curl "http://$(terraform output |grep IP| sed 's/IP//g'|sed 's/,//g'|sed 's/ //g'|sed 's/"//'|sed 's/"//')"
