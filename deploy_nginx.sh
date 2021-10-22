a0919a9df7ade5285cd76b31e05e97143356744d
Mmota <marcelo.rmota@hotmail.com>
2021-10-22 17:51:02 +0000

    adicionando criação de image

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -1,6 +1,6 @@
 cd terraform
 echo [nginx] >  /var/lib/jenkins/workspace/PipeMota/ansible/hosts
-terraform output |grep ec2 >> /var/lib/jenkins/workspace/PipeMota/ansible/hosts
+terraform output |grep IP| sed 's/IP//g'|sed 's/,//g'|sed 's/ //g' >> /var/lib/jenkins/workspace/PipeMota/ansible/hosts
 
 cd /var/lib/jenkins/workspace/PipeMota/ansible
 ansible-playbook -i hosts nginx.yml -u ubuntu 
