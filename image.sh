52e752ca427932d7a1cf3a547ef7e46d6d46f70a
Mmota <marcelo.rmota@hotmail.com>
2021-10-22 18:32:30 +0000

    Alterações no image

--- image.sh
+++ image.sh
@@ -1,11 +1,14 @@
+mkdir image
+
 cd terraform
 IMAGE=$(terraform output| grep "i-"|sed  's/,//g')
 
-cat <<EOF > print.tf
+cat <<EOF > ../image/print.tf
 resource "aws_ami_from_instance" "nginx_mmota" {
   name               = "nginx_mmota"
   source_instance_id = ${IMAGE}
 }
 EOF
-
+cd ../image/
+terraform init
 terraform apply -auto-approve
