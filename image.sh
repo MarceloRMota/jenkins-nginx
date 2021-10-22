f8f0b20ab3213750ed0220786446f95eb4d7d22f
Mmota <marcelo.rmota@hotmail.com>
2021-10-22 19:01:02 +0000

    Alterações no image

--- image.sh
+++ image.sh
@@ -4,6 +4,10 @@ cd terraform
 IMAGE=$(terraform output| grep "i-"|sed  's/,//g')
 
 cat <<EOF > ../image/print.tf
+provider "aws" {
+  region = "sa-east-1"
+}
+
 resource "aws_ami_from_instance" "nginx_mmota" {
   name               = "nginx_mmota"
   source_instance_id = ${IMAGE}
